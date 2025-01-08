5a582dda9793bb829e8d8361eee8adf211156169
Satyanshu Bhardwaj <satyanshu.bhardwaj123@gmail.com>
2025-01-08 10:08:07 +0000

    06_nums_and_math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const scroe = 400
+// console.log(scroe);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const anotherNumber = 1123.8545
+// console.log(anotherNumber.toPrecision(3));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// *************** MATHS ****************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2, 4, 5, -6, 9));
+// console.log(Math.max(2, 4, 5, -6, 9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min +1) + min))
+
+
