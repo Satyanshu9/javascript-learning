fceea1bfb26e1e8e7e2ce823cd27b3caf4452fa0
Satyanshu Bhardwaj <satyanshu.bhardwaj123@gmail.com>
2025-02-17 13:09:42 +0530

    Update 04_comparison.js
    added comments

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -4,16 +4,19 @@ console.log(2<1);
 console.log(1 == 1);
 console.log(2 != 1);
 
-console.log("2" > 1);
-console.log("02" > 1);
+console.log("2" > 1); //true
+console.log("02" > 1); //true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+//the reason that equality check == and comparisons <><= work differently
+//coparisons convert null to a number , 0 . thats why the above results occur
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
 
 //strict check ===
-console.log("2" == 2);
+console.log("2" == 2); //true
+console.log("2" === 2); //false
